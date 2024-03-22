a1de886eca1aac81b67896c0f8ae334054fcaa67
Eduardo Fatec <eduardokamoz@gmail.com>
2024-03-22 20:40:19 -0300

    Corrigido exercício 07

--- AULA05A/ex07.py
+++ AULA05A/ex07.py
@@ -1,24 +1,24 @@
-combustivel = input('Digite o código do combustivel (A-álcool, G-gasolina): ').lower()
+combustivel = input('Digite o código do combustivel (A-álcool, G-gasolina): ')
 litros_vendidos = int(input('Digite quantos litros foram vendidos: '))
 
 valor_total = float
 
-match combustivel:
+match combustivel.lower():
     case 'a':
         valor_bruto = litros_vendidos * 1.9
         if litros_vendidos <= 20:
             desconto = valor_bruto * 0.03
-            valor_total = litros_vendidos - desconto
+            valor_total = valor_bruto - desconto
         else:
             desconto = valor_bruto * 0.05
-            valor_total = litros_vendidos - desconto
-    case 'b':
+            valor_total = valor_bruto - desconto
+    case 'g':
         valor_bruto = litros_vendidos * 2.5
         if litros_vendidos <= 20:
             desconto = valor_bruto * 0.04
-            valor_total = litros_vendidos - desconto
+            valor_total = valor_bruto - desconto
         else:
             desconto = valor_bruto * 0.06
-            valor_total = litros_vendidos - desconto
+            valor_total = valor_bruto - desconto
 
 print(valor_total)
